145f5e87f08231257c98d3440768a358614a419c
oni-97 <oni.ils.thou@gmail.com>
2021-09-06 13:38:46 +0900

    マイページ編集

--- app/src/main/java/com/example/androidenshugroup2/MyPageActivity.java
+++ app/src/main/java/com/example/androidenshugroup2/MyPageActivity.java
@@ -7,42 +7,40 @@ import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MyPageActivity extends AppCompatActivity {
+    private String userName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.my_page_activity);
 
+        //遷移元のアクティビティから「ジャンル (String型)」を受け取る。
+        //タイトルをジャンルに応じて設定
+        Intent intentMain = getIntent();
+        userName = intentMain.getStringExtra(MainActivity.USER_NAME);
+
         //　商品購入に遷移
         Button mpiBtn = findViewById(R.id.mv_purchase_item_btn);
         mpiBtn.setOnClickListener(v-> {
             Intent intent = new Intent(getApplication(), PurchaseItemActivity.class);
+            intent.putExtra(MainActivity.USER_NAME, userName);
             startActivity(intent);
         });
 
-        /* 購入履歴に遷移
+        // 購入履歴に遷移
         Button mphBtn = findViewById(R.id.mv_purchase_history_btn);
         mphBtn.setOnClickListener(v-> {
-            Intent intent = new Intent(getApplication(), .class);
+            Intent intent = new Intent(getApplication(), HistoryActivity.class);
+            intent.putExtra(MainActivity.USER_NAME, userName);
             startActivity(intent);
         });
-         */
 
-        /* 設定に遷移
-        Button msBtn = findViewById(R.id.mv_setting_btn);
-        msBtn.setOnClickListener(v-> {
-            Intent intent = new Intent(getApplication(), .class);
-            startActivity(intent);
-        });
-         */
-
-        /* 商品登録に遷移
+        // 商品登録に遷移
         Button mriBtn = findViewById(R.id.mv_register_item_btn);
         mriBtn.setOnClickListener(v-> {
-            Intent intent = new Intent(getApplication(), .class);
+            Intent intent = new Intent(getApplication(), RegisterPage.class);
             startActivity(intent);
         });
-        */
 
         // ログアウトに遷移
         Button mlBtn = findViewById(R.id.mv_logout_btn);

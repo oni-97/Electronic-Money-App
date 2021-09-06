fa5503d6e073c100bae7feb5f90c03de2306605d
oni-97 <oni.ils.thou@gmail.com>
2021-09-06 15:55:00 +0900

    パッケージ名修正

--- app/src/main/java/com/example/androidenshugroup2/St.java
+++ app/src/main/java/com/example/androidenshugroup2/St.java
@@ -1,4 +1,4 @@
-package com.example.myapplication;
+package com.example.androidenshugroup2;
 
 public class St {
     private int id;
--- app/src/main/java/com/example/androidenshugroup2/ZhuActivity.java
+++ app/src/main/java/com/example/androidenshugroup2/ZhuActivity.java
@@ -1,4 +1,4 @@
-package com.example.myapplication;
+package com.example.androidenshugroup2;
 
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;

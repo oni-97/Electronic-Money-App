355c1e3a114ba24341a7cacfd7a541c75b12de3c
da-hayas <da-hayas@ist.osaka-u.ac.jp>
2021-09-28 00:19:25 +0900

    HistoryActivityが反映されてなかったので

--- app/src/main/java/com/example/androidenshugroup2/HistoryAdapter.java
+++ app/src/main/java/com/example/androidenshugroup2/HistoryAdapter.java
@@ -1,2 +1,64 @@
-package com.example.androidenshugroup2;public class HistoryAdapter {
+package com.example.androidenshugroup2;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.androidenshugroup2.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HistoryAdapter extends RecyclerView.Adapter<HistoryViewHolder> {
+
+    private static List<PurchaseData> list;
+    private  onItemClickListener listener;
+    public HistoryAdapter(List<PurchaseData> list) {
+        this.list = list;
+    }
+
+    @Override
+    public HistoryViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View inflate = LayoutInflater.from(parent.getContext()).inflate(R.layout.history_data, parent,false);
+        HistoryViewHolder vh = new HistoryViewHolder(inflate);
+        vh.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final int position = vh.getAdapterPosition();//位置取得
+                final int id =list.get(position).getId();//ID取得
+                listener.onClick(v,id,position);
+            }
+
+        });
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull HistoryViewHolder holder, int position) {
+        holder.historyDateView.setText(String.valueOf(list.get(position).getPurchaseDateTime().getMonthValue())
+                + "/" + String.valueOf(list.get(position).getPurchaseDateTime().getDayOfMonth()));
+        holder.historyItemView.setText(list.get(position).getItemName());
+        holder.historyPriceView.setText(String.valueOf(list.get(position).getPurchasePrice()) + "円");
+    }
+    public void setOnItemClickListener(onItemClickListener listener) {
+        this.listener = listener;
+    }
+
+
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public interface onItemClickListener {
+        void onClick(View view, int id,int position);
+    }
+
 }
